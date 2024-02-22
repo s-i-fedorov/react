34a8d8832df983b2c13d2b5fee1499c8c21ea9e5
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-02-22 23:09:39 +0200

    hw_72 try react

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,34 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+         <h1>My first react app</h1>
+      </header>
+        <main className="container">
+<nav className="sidebar">
+    <button>some button</button>
+    <button>some button</button>
+    <button>some button</button>
+    <button>some button</button>
+</nav>
+            <section className="content">Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Dicta laborum necessitatibus nulla quasi ullam?
+                Ad assumenda beatae doloremque maiores minima numquam quasi quisquam
+                saepe sed, sunt suscipit veritatis vero voluptas.Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Dicta laborum necessitatibus nulla quasi ullam?
+                Ad assumenda beatae doloremque maiores minima numquam quasi quisquam
+                saepe sed, sunt suscipit veritatis vero voluptas.Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Dicta laborum necessitatibus nulla quasi ullam?
+                Ad assumenda beatae doloremque maiores minima numquam quasi quisquam
+                saepe sed, sunt suscipit veritatis vero voluptas.Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Dicta laborum necessitatibus nulla quasi ullam?
+                Ad assumenda beatae doloremque maiores minima numquam quasi quisquam
+                saepe sed, sunt suscipit veritatis vero voluptas.Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Dicta laborum necessitatibus nulla quasi ullam?
+                Ad assumenda beatae doloremque maiores minima numquam quasi quisquam
+                saepe sed, sunt suscipit veritatis vero voluptas.</section>
+        </main>
+
     </>
   )
 }
