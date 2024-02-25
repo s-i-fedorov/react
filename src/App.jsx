af34263b66887cdeaa573e71de1a0343c8f1944d
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-02-25 20:50:48 +0200

    hw_72 completed

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,12 @@ function App() {
          <h1>My first react app</h1>
       </header>
         <main className="container">
-<nav className="sidebar">
-    <button>some button</button>
-    <button>some button</button>
-    <button>some button</button>
-    <button>some button</button>
-</nav>
+            <nav className="sidebar">There is a sidebar
+                <button>some button</button>
+                <button>some button</button>
+                <button>some button</button>
+                <button>some button</button>
+            </nav>
             <section className="content">Lorem ipsum dolor sit amet, consectetur
                 adipisicing elit. Dicta laborum necessitatibus nulla quasi ullam?
                 Ad assumenda beatae doloremque maiores minima numquam quasi quisquam
