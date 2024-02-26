f875b1efa852568cbb3c6303b2e9b1b669686631
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-02-27 00:23:32 +0200

    hw_73 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import Card from './components/Card.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <header>
          <h1>My first react app</h1>
       </header>
         <main className="container">
-            <nav className="sidebar">There is a sidebar
-                <button>some button</button>
-                <button>some button</button>
-                <button>some button</button>
-                <button>some button</button>
-            </nav>
-            <section className="content">Lorem ipsum dolor sit amet, consectetur
-                adipisicing elit. Dicta laborum necessitatibus nulla quasi ullam?
-                Ad assumenda beatae doloremque maiores minima numquam quasi quisquam
-                saepe sed, sunt suscipit veritatis vero voluptas.Lorem ipsum dolor sit amet, consectetur
-                adipisicing elit. Dicta laborum necessitatibus nulla quasi ullam?
-                Ad assumenda beatae doloremque maiores minima numquam quasi quisquam
-                saepe sed, sunt suscipit veritatis vero voluptas.Lorem ipsum dolor sit amet, consectetur
-                adipisicing elit. Dicta laborum necessitatibus nulla quasi ullam?
-                Ad assumenda beatae doloremque maiores minima numquam quasi quisquam
-                saepe sed, sunt suscipit veritatis vero voluptas.Lorem ipsum dolor sit amet, consectetur
-                adipisicing elit. Dicta laborum necessitatibus nulla quasi ullam?
-                Ad assumenda beatae doloremque maiores minima numquam quasi quisquam
-                saepe sed, sunt suscipit veritatis vero voluptas.Lorem ipsum dolor sit amet, consectetur
-                adipisicing elit. Dicta laborum necessitatibus nulla quasi ullam?
-                Ad assumenda beatae doloremque maiores minima numquam quasi quisquam
-                saepe sed, sunt suscipit veritatis vero voluptas.</section>
+            <Card />
+            <Card />
         </main>
 
     </>
