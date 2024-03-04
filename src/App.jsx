d0bd6df953eb58ae70676881c07716b19bab1f52
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-03-04 23:13:10 +0200

    hw_77

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
+import ListGroup from "./components/ListGroup.jsx";
 
 function App() {
 
   return (
-
+      <>
+        <header>
+          <h1>My first react app</h1>
+        </header>
+        <ListGroup >
+          <p>one</p>
+          <p>two</p>
+        </ListGroup>
+      </>
 
   )
 }
