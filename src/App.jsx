f09604822dcc547094e70716c2036cb44986c9a6
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-03-04 21:31:25 +0200

    hw_75

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Progress from "./components/Progress.jsx";
 
 function App() {
 
   return (
-
-
+      <>
+        <header>
+          <h1>My first react app</h1>
+        </header>
+      < Progress />
+</>
   )
 }
 
--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,5 +1,18 @@
+import React from "react";
 
-<div className="progress">
-    <div className="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" aria-label="progressbar" style="width: 40%;">
-    </div>
-</div>
+class Progress extends React.Component{
+    // percentage = this.props.percentage;
+    // text = this.props.text;
+
+    render(){
+        return      <div className="progress">
+                        <div className="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0"
+                             aria-valuemax="100" aria-label="progressbar" style="width: 40%;">
+                        </div>
+                    </div>
+    }
+}
+
+export default Progress;
+
+
