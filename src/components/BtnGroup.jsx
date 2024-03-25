f0c8ccf7805e9840fe4f279dde1324e55a98da58
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-03-25 23:19:47 +0200

    hw_78 doesn't work

--- src/components/BtnGroup.jsx
+++ src/components/BtnGroup.jsx
@@ -1,15 +1,40 @@
 import React from "react";
+import cn from 'classnames';
 
 class BtnGroup extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            // isPressed: false,
+            btnClasses: 'btn btn-secondary'
+        }
+    }
 
+    handleClick = () => {
+        this.setState({ bntClasses: 'btn btn-primary' })
+        console.log(this.state)
+}
 
     render(){
-        const a = this.props;
+        // let isPressed = false;
+
+        // const btnClasses = cn('btn', {
+        //     'btn-secondary': !this.state.isPressed,
+        //     'btn-primary': this.state.isPressed
+        // })
+
+        // let btnClasses2 = 'btn btn-secondary'
+        // const pressHandl = function (){
+        //     btnClasses2 = 'btn btn-primary'
+        //     console.log('hello');
+        //     return btnClasses2
+        // }
+
         return (
 
             <div className="btn-group" role="group">
-                <button type="button" className="btn btn-secondary left">Left</button>
-                <button type="button" className="btn btn-secondary right">Right</button>
+                <button type="button" className={this.state.btnClasses + ' left'} onClick={this.handleClick}>Left</button>
+                <button type="button" className={this.state.btnClasses + ' right'} >Right</button>
             </div>
 
         )
